13548c423a53db8079acaac0ee80c57542989980
Ayanavbha Chatterjee <117476380+aYgCOO@users.noreply.github.com>
2024-07-13 17:14:43 +0530

    New Update
    Incomplete Debugging

--- .js endgame/js/Asynchronous.js
+++ .js endgame/js/Asynchronous.js
@@ -14,73 +14,147 @@ A good example is JavaScript setTimeout()
 */
 
 
-//setTimeout
+/* setTimeout */
 
-setTimeout(function(){
+setTimeout(function () {
      console.log("First Execution");
-},5000)
+}, 5000)
 
 console.log(" Second Execution");
 
-//Promises
+/* Promises */
 
- // Sync execution
-const promisesOne = new Promise(function(resolve, reject){
+// Promise resolve
+// Sync execution 
+const promisesOne = new Promise(function (resolve, reject) {
      console.log("Sync,task execute successfully");
-     resolve(); 
+     resolve();
 })
 promisesOne.then(
-     function(){
+     function () {
           console.log("Sync, task promise consume successfully");
      }
 )
 
- // Async execution
-const promisesTask = new Promise(function(resolve, reject){
-     setTimeout(function(){
-         console.log("Async, task completed successfully");
-         resolve(); 
-   }, 5000)
-  
- })
- promisesTask.then(function(){
-     setTimeout(function(){
-         console.log("Async, promise consume successfully");
-     },2000)
- })
+// Async execution
+const promisesTask = new Promise(function (resolve, reject) {
+     setTimeout(function () {
+          console.log("Async, task completed successfully");
+          resolve();
+     }, 5000)
+
+})
+promisesTask.then(function () {
+     setTimeout(function () {
+          console.log("Async, promise consume successfully");
+     }, 2000)
+})
 
 
 // Data retrieval 
-const dataRetrieval = new Promise(function(resolve, reject){
-     setTimeout(function(){
+const dataRetrieval = new Promise(function (resolve, reject) {
+     setTimeout(function () {
           console.log("Data retrieve successfully");
           resolve({
                username: "ayanabha",
                email: "demo3ayanabha@gmail.com",
-               MobileNo: 9875682351  
+               MobileNo: 9875682351
           })
      }, 6000)
 
- })
- dataRetrieval.then(function(userData){
+})
+dataRetrieval.then(function (userData) {
      console.log("Please wait.......")
-     setTimeout(function(){
-          console.log("Data fatching..... ");
-          setTimeout(function(){
+     setTimeout(function () {
+          console.log("Data fetching..... ");
+          setTimeout(function () {
                console.log(userData);
                console.log("Data retrieval promise consume successfully");
-               
+
           }, 2000)
      }, 10000)
-    
- })
+
+})
+
+//Promise rejects
+//Async execution 
+const FileAccess = new Promise(function (resolve, reject) {
+     setTimeout(function () {
+          var error = false;
+          if (!error) {
+               console.log("Execution Successful ✅");
+               resolve({
+                    FileName: "abcd.exe",
+                    FilePath: "C:\\User\\System32\\nosexos\\abcd.exe",
+                    FileExtension: ".exe",
+
+               });
+          } else {
+               console.log("Execution Reject ❌");
+               reject({ ERROR: "Oops! Something went wrong : (" });
+          }
+     }, 3000)
+});
+
+FileAccess.then(function (fileDetails) {
+     setTimeout(function () {
+          console.log("Procession.....");
+          setTimeout(function () {
+               console.log("File Details:");
+               console.log(fileDetails);
+
+          }, 1000)
+     }, 5000)
+     return fileDetails;
+}).then(function (fileDetails) {
+     console.log(" {abcd.exe}:" + fileDetails.FileName);
+     console.log("Access Grant ✅");       /*In JavaScript, promises allow chaining of then methods, which helps to handle asynchronous operations sequentially.                            
+                                             Each then method returns a new promise, and the value returned from the callback function within a then method is                       
+                                             passed to the next then method in the chain.  */            
+}).catch(function (fileAccessDenied) {
+     setTimeout(function () {
+          console.log("{ERROR}:" + fileAccessDenied.ERROR);
+          console.log("Access Denied ❌")
+
+     }, 0)
+}).finally(function(){
+     setTimeout(function(){
+          console.log("Session End");
+     },8000)
+})
 
 
+// Promises using Async & Await 
 
+const finalPromise = new Promise((resolve, reject)=>{
 
+     setTimeout(() => {
+          var er = true;
+          if(!er){
+               console.log("SUCCESS");
+               resolve({Username: "Sristi Sarker", Gender: "Female" });
+
+          }else{
+               console.log("ERROR");
+               reject("Unknown");
+          }
+          
+     }, 2000);
+})
+
+async function AsyncPromiseConsumer(){
+     try{
+          console.log("Execution Successfully");
+          let response = await finalPromise
+          console.log(response);
+     }catch(Unknown){
+          console.log("Execution Reject");
+          console.log(Unknown);
+     } // Incomplete debugging 
+} 
 // setInterval & clearInterval
 
-const ThisInterval = function(str){
+const ThisInterval = function (str) {
      console.log(str, "Current Date: ", Date.now());
 }
 
