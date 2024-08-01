2048256260b119268f1e20927f4be24401bed5ec
Ayanavbha Chatterjee <117476380+aYgCOO@users.noreply.github.com>
2024-08-01 18:50:16 +0530

    All file remove

--- .js endgame/js/Asynchronous/Interval.js
+++
@@ -1,8 +0,0 @@
-// setInterval & clearInterval
-const ThisInterval = function (str) {
-     console.log(str, "Current Date: ", Date.now());
-}
-
-clearM = setInterval(ThisInterval, 2000, "Hi Bc");
-
-clearInterval(clearM);
